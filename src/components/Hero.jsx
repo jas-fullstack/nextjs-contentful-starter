74691bc12a77b4b09022d51dff249b6ff2ceb516
jaswinder Kumar <kumarsanju54@gmail.com>
2024-09-15 07:46:13 +0000

    update home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,10 @@ const themeClassMap = {
 export const Hero = (props) => {
   return (
     <div className="px-6 py-16 bg-gray-100 sm:px-12 sm:py-24" data-sb-object-id={props.id}>
-      <div className={`max-w-6xl mx-auto flex flex-col gap-12 md:items-center ${themeClassMap[props.theme] ?? themeClassMap['imgRight']}`}>
+      <div>
+        here we can put all html
+      </div>
+      {/* <div className={`max-w-6xl mx-auto flex flex-col gap-12 md:items-center ${themeClassMap[props.theme] ?? themeClassMap['imgRight']}`}>
         <div className="w-full max-w-xl mx-auto flex-1">
           <h1 className="mb-6 text-4xl font-bold sm:text-5xl" data-sb-field-path="heading">
             {props.heading}
@@ -34,7 +37,7 @@ export const Hero = (props) => {
             />
           )}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
